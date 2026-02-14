12160e60264c68ace8fb1b689a492e89106dadb4
René Backhaus <rene.backhaus@real-digital.de>
2026-02-14 12:17:42 +0100

    add a wordlist to test the hash function on collisions

--- hashmap/main.go
+++ hashmap/main.go
@@ -1,11 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	s := "abc"
-	hash := hash(s)
-	fmt.Println(hash)
+	readLinesFromFileAndHash()
+}
+
+func readLinesFromFileAndHash() error {
+	m := make(map[int]int)
+	fileHandle, err := os.Open("wordlist.txt")
+	if err != nil {
+		return err
+	}
+	defer fileHandle.Close()
+	scanner := bufio.NewReader(fileHandle)
+	for {
+		textLine, err := scanner.ReadString('\n')
+		if err == io.EOF {
+			if len(textLine) != 0 {
+				hashedTextline := hash(textLine)
+				m[int(hashedTextline)] = m[int(hashedTextline)] + 1
+			}
+			break
+		}
+		if err != nil {
+			return fmt.Errorf("error reading from file: %w", err)
+		}
+		hashedTextline := hash(textLine)
+		m[int(hashedTextline)] = m[int(hashedTextline)] + 1
+	}
+
+	displayResults(m)
+
+	return nil
+}
+
+func displayResults(m map[int]int) {
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
 }
 
 func hash(s string) uint32 {
